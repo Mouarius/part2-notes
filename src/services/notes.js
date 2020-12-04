cce6780e7ea4c3cc3483e68de5eb0a46fdf07237
mouarius <mouarius@gmail.com>
2020-12-04 12:05:30 +0100

    Link to backend app

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://evening-mountain-35874.herokuapp.com/api/notes/'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
